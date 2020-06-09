5300fc1d8f2f97fcc41c2e4bc786bd6288bbf82b
tristan straub <tristan@straub.id.au>
2020-06-10 09:44:46 +1000

    Run nrepl server, with ui-main on main thread

--- src/blender_clj/nrepl.clj
+++
@@ -1,6 +0,0 @@
-(ns blender-clj.nrepl
-  (:require [nrepl.server :refer [start-server stop-server]]
-            [blender-clj.core :refer [ui-main]]))
-
-(println "NREPL started on port:" (:port (start-server)))
-(ui-main)
