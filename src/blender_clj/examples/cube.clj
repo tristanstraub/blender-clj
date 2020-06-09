cdf4717774a6d5fceddfc4e0b5ce5339b8b984f5
tristan straub <tristan@straub.id.au>
2020-06-10 09:46:19 +1000

    Revert example formatting

--- src/blender_clj/examples/cube.clj
+++ src/blender_clj/examples/cube.clj
@@ -8,8 +8,7 @@
     (blender/with-context
       (fn [defaults]
         (dotimes [_ 10]
-          (py.. bpy -ops -mesh (primitive_cube_add defaults :size 3 :location (vec (repeatedly 3 #(- (rand-int 20) 10))))))
-        ))))
+          (py.. bpy -ops -mesh (primitive_cube_add defaults :size 3 :location (vec (repeatedly 3 #(- (rand-int 20) 10))))))))))
 
 (comment
   (demo)
